b2b160ee2d725fec6bc267d65d279fd10620ec2b
João Vitor Oliveira da Silva <100168573+jvitsilva@users.noreply.github.com>
2022-06-14 19:50:16 -0300

    Create formulario.py

---
+++ formulario.py
@@ -0,0 +1,25 @@
+'''
+Exercício:
+Faça um formulário que pergunte o nome, cpf, endereço,
+idade, altura e telefone. Imprima isso em um relatório
+organizado.
+'''
+
+print('Bem vindo à Python inc! Vou fazer algumas perguntas')
+
+
+nome = input('Seu nome:')
+cpf = input('CPF:')
+Endereço = input('Endereço:')
+idade = input('idade: ')
+altura = input('altura:')
+telefone = input('telefone:')
+print('\n\n')
+
+print('Está correto?')
+print('Nome: ', nome)
+print('CPF: ', cpf)
+print('Endereço: ', Endereço)
+print('Idade: ', idade)
+print('Altura: ', altura)
+print('Telefone: ', telefone)
